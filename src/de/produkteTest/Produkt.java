ff58560160e4b7e052b9fc36002ccf55c8144ecd
preuschhi <bmx.lego@gmail.com>
2020-10-01 19:25:55 +0200

    Update Produkt.java

--- src/de/produkteTest/Produkt.java
+++ src/de/produkteTest/Produkt.java
@@ -1,27 +1,30 @@
 package de.produkteTest;
 
-
 import java.util.*;
+import java.util.Map.Entry;
+
+
 
 public class Produkt {
 
 	// Eigenschaften
-	private String produktNameString;
-	private int produktNummer;
-	private double produktPreis;
-	private int produktMHD;
-	private int produktIndex;
+	String produktNameString;
+	int produktNummer;
+	double produktPreis;
+	int produktMHD;
 	FehlerTest fehlerTest;
+	// In dieser Map befinden sich alle Produkte;
 	static Map<Integer, Produkt> mapProdukteMap = new LinkedHashMap<>();
-	public static int index = mapProdukteMap.size();
+	// true = Produkt wird ueberall geloescht;
+	// false = Produkt wird in bestimmtem Kunden geloescht;
+	static boolean woBefindetSichDasZuLoeschendeProdukt;
 
 	// Konstruktor
-	public Produkt(String produktNameString, int produktNummer, double produktPreis, int produktMHD, int produktIndex) {
+	public Produkt(String produktNameString, int produktNummer, double produktPreis, int produktMHD) {
 		this.produktNameString = produktNameString;
 		this.produktNummer = produktNummer;
 		this.produktPreis = produktPreis;
 		this.produktMHD = produktMHD;
-		this.produktIndex = produktIndex;
 	}
 
 	// Methoden
@@ -34,7 +37,8 @@ public class Produkt {
 
 		Scanner scan = new Scanner(System.in);
 		boolean testSchleife = false;
-		//Diese Variablen dienen den Methoden, das sie sie nicht selber initialisieren muessen;
+		// Diese Variablen dienen den Methoden, das sie sie nicht selber initialisieren
+		// muessen;
 		int eingabeInt = 0;
 		String eingabeString = null;
 		double eingabeDouble = 0.0d;
@@ -51,8 +55,7 @@ public class Produkt {
 		// Abfrage fuer die ProduktMHD Dauer
 		int produktMHD = setProduktMHD(testSchleife, scan, eingabeString);
 
-		Produkt newProdukt = new Produkt(produktNameString, produktNummer, produktPreis, produktMHD,
-				mapProdukteMap.size());
+		Produkt newProdukt = new Produkt(produktNameString, produktNummer, produktPreis, produktMHD);
 
 		// Das newProdukt wird zu listProdukteList hinzugefuegt damit es fuer weitere
 		// Schritte genutzt werden kann
@@ -71,7 +74,7 @@ public class Produkt {
 		// Man kann eine bestimmtes Produkt auswaehlen und anzeigen lassen
 		boolean testSchleife = false;// Dieser bool steuert die Testschleife;true = Abgrage wird wiederholt false =
 										// Der Vorgang wurde Abgebrochen oder die Eingabe war Korrekt;
-		boolean produktWirdGeholt = true;//Dieser bool aktiviert die Schleife welche das gesuchte Produkt returnt;
+		boolean produktWirdGeholt = true;// Dieser bool aktiviert die Schleife welche das gesuchte Produkt returnt;
 		Produkt geholtesProdukt = null;// In diesem Objekt wird das gewuenschte Produkt gespeichert;
 		String eingabeString = null;// Hier wird die Usereingabe gespeichert;
 		Scanner scanner = new Scanner(System.in);
@@ -83,13 +86,13 @@ public class Produkt {
 		 */
 		do {
 			System.out.println("Welches Produkt soll gewählt werden?\nBitte Produktnummer eingeben\n[0]Abbrechen");
-			showMapProdukteMap();
 			eingabeString = scanner.next();
-			testSchleife = FehlerTest.tryCatchInteger(eingabeString, true, false, 0, 0, true, false, false);
+			testSchleife = FehlerTest.tryCatchInteger(eingabeString, true, false, 0, 0, true, false, false, false,
+					false, false);
 			if (eingabeString.equals("0")) {
 				produktWirdGeholt = false;
-				//testSchleife wird extra false gesetzt da tryCatchInteger sie auf true stellt;
-				testSchleife = false; 
+				// testSchleife wird extra false gesetzt da tryCatchInteger sie auf true stellt;
+				testSchleife = false;
 			}
 		} while (testSchleife == true);
 
@@ -117,27 +120,28 @@ public class Produkt {
 	public static String setProduktNameString(boolean testSchleife, String eingabeString, Scanner scan) {
 		// produktName
 		/*
-		 * Diese Methode gibt dem erstellten Produkt den Produktnamen;
-		 * In einer tryCatchStringMethode wird getestet ob die Usereingabe korrekt ist;
+		 * Diese Methode gibt dem erstellten Produkt den Produktnamen; In einer
+		 * tryCatchStringMethode wird getestet ob die Usereingabe korrekt ist;
 		 */
 		do {
 			System.out.println("Wie soll das Produkt heißen?");
 			eingabeString = scan.next();
-			testSchleife = FehlerTest.tryCatchString(eingabeString);
+			testSchleife = FehlerTest.tryCatchString(eingabeString, false, false);
 		} while (testSchleife == true);
 		return eingabeString;
 	}
 
 	public static int setProduktNummer(boolean testSchleife, String eingabeString, Scanner scan) {
-		// Produktnummer	
+		// Produktnummer
 		/*
-		 * Diese Methode gibt dem erstellten Produkt die Produktnummer; 
-		 * In einer trycatchMehtode wird getestet ob die Usereingabe Korrekt ist;
+		 * Diese Methode gibt dem erstellten Produkt die Produktnummer; In einer
+		 * trycatchMehtode wird getestet ob die Usereingabe Korrekt ist;
 		 */
 		do {
 			System.out.println("Produktnummer: ___");
 			eingabeString = scan.next();
-			testSchleife = FehlerTest.tryCatchInteger(eingabeString, false, false, 0, 0, false, true, false);
+			testSchleife = FehlerTest.tryCatchInteger(eingabeString, false, false, 0, 0, false, true, false, false,
+					false, false);
 		} while (testSchleife == true);
 		return Integer.parseInt(eingabeString);
 	}
@@ -156,9 +160,236 @@ public class Produkt {
 		do {
 			System.out.println("ProduktMHD dauer: ___ in Tagen");
 			eingabeString = scan.next();
-			testSchleife = FehlerTest.tryCatchInteger(eingabeString, false, false, 0, 0, false, false, false);
+			testSchleife = FehlerTest.tryCatchInteger(eingabeString, false, false, 0, 0, false, false, false, false,
+					false, false);
 		} while (testSchleife == true);
 		return Integer.parseInt(eingabeString);
+	}
+
+	public static void deleteProdukt(boolean testSchleife, Scanner scan, String eingabeString, int kundenNummer) {
+
+		System.out.println("Welches Produkt willst du loeschen?");
+		do {
+			System.out.println("Produktnummer:___\n[0]Abbruch");
+			eingabeString = scan.nextLine();
+			testSchleife = FehlerTest.tryCatchInteger(eingabeString, true, false, 0, 0, true, false, false, false,
+					false, false);
+		} while (testSchleife == true);
+
+		// true = Produkt wird ueberall geloescht;
+		// false = Produkt wird in bestimmtem Kunden geloescht;
+		/*
+		 * Diesen bool findet man in:
+		 * Kunde.ausgabeKundeWahl();
+		 * Funktionen.steuerungProduktMenu();
+		 * 
+		 * Wenn man z.B. in den KundenOptionen ist und einen Kunden augeben will,
+		 * wird der bool auf false gestellt, d.h. das Produkt wurd NUR fuer diesen Kunden
+		 * geloescht;
+		 * 
+		 * Wenn man aber in Funktionen.steuerungProduktMenu() ein Produkt loeschen will,
+		 * wird der bool auf true gestellt, d.h das Produkt wird fuer jeden Kunden und 
+		 * allgemein aus allen Strukturen entfernt;
+		 */
+		if (!eingabeString.equals("0")) {
+			if (woBefindetSichDasZuLoeschendeProdukt == true) {
+				deleteProduktKomplett(testSchleife, scan, eingabeString);
+				System.out.println("Das Produkt wurde unwiederruflich geloescht");
+			}
+			if (woBefindetSichDasZuLoeschendeProdukt == false) {
+				deleteProduktVonKunde(testSchleife, scan, eingabeString, kundenNummer);
+			}
+		}
+	}
+
+	public static void deleteProduktVonKunde(boolean testSchleife, Scanner scan, String eingabeString,
+			int kundenNummer) {
+		/*
+		 * Diese Methode dursucht mithilfe eines Iterators die mapKundenProdukte des
+		 * Kunden von dem das Produkt geloescht werden soll;
+		 */
+		/*
+		 * Wenn dieser bool true bliebt, bedeutet dies, das der Kunde das zu loeschende
+		 * Produkt nicht besitzt;
+		 */
+		boolean kundeBesitztDasProduktNicht = true;
+		Iterator<Entry<Integer, Produkt>> iterator = KundenStrukturen.mapAlleKundenMap
+				.get(kundenNummer).mapKundenProdukte.entrySet().iterator();
+		while (iterator.hasNext()) {
+			if (Integer.parseInt(eingabeString) == iterator.next().getKey()) {
+				iterator.remove();
+				kundeBesitztDasProduktNicht = false;
+			}
+		}
+		if (kundeBesitztDasProduktNicht == true) {
+			System.out.println("Der Kunde hat das zu loeschende Produkt nicht in seinem Inventar");
+		}
+	}
+
+	public static void deleteProduktKomplett(boolean testSchleife, Scanner scan, String eingabeString) {
+		/*
+		 * Diese Methode durchsucht mithilfe einer for-each-Schleife mapAlleKundenMap;
+		 * Im Kunden wird dann ein Iterator mapKundenProdukte nach dem zu loeschenden
+		 * Produkt durchsuchen; Wenn der Kunde das Produkt besitzt, wird es geloescht;
+		 * Zudem wird das Produkt auch in mapProdukteMap geloescht, somit ist es
+		 * komplett verloren;
+		 */
+		// Es wird jeder Kunde durchsucht;
+		for (Integer keyKunden : KundenStrukturen.mapAlleKundenMap.keySet()) {
+			/*
+			 * Die Map in der die Produkte eines Kunden gespeichert sind werden durchsucht;
+			 * Wenn der Kunde das gewuenschte Produkt besitzt, wird es in einem Iterator
+			 * geloescht;
+			 */
+			Iterator<Entry<Integer, Produkt>> iterator = KundenStrukturen.mapAlleKundenMap
+					.get(keyKunden).mapKundenProdukte.entrySet().iterator();
+			while (iterator.hasNext()) {
+				if (Integer.parseInt(eingabeString) == iterator.next().getKey()) {
+					iterator.remove();
+				}
+			}
+			Iterator<Entry<Integer, Produkt>> iterator2 = mapProdukteMap.entrySet().iterator();
+			while (iterator2.hasNext()) {
+				if (Integer.parseInt(eingabeString) == iterator2.next().getKey()) {
+					iterator2.remove();
+				}
+			}
+
+		}
+	}
+
+	public static void editProduktWahl(boolean testSchleife, Scanner scan, String eingabeString) {
+		/*
+		 * In dieser Methode kann der User eine Produkt auswaehlen und editieren; Als
+		 * erstes wird durch eine doWhileSchleife das Produkt ausgewaehlt; Dann wird
+		 * eine Kopie (changingProdukt) von diesen Produkt erstellt => Diese Kopie wird
+		 * zum editieren verwendet; In editProdukt() wird changingProdukt editiert; In
+		 * deleteAndUpdateChangingProduktToKunden() wird das "alte" Produkt aus den
+		 * Kunden entfernt und durch changingProdukt ersetzt;
+		 */
+		System.out.println("Welches Produkt willst du bearbeiten?");
+		do {
+			System.out.println("Produktnummer:___\n[0]");
+			eingabeString = scan.nextLine();
+			testSchleife = FehlerTest.tryCatchInteger(eingabeString, true, false, 0, 0, true, false, false, false,
+					false, false);
+		} while (testSchleife == true);
+
+		if (!eingabeString.equals("0")) {
+			// Hier wird die Kopie vom Produkt erstellt;
+			Produkt changingProdukt = mapProdukteMap.get(Integer.parseInt(eingabeString));
+			// Hier wird das Produkt editiert;
+			changingProdukt = editProdukt(testSchleife, scan, eingabeString, changingProdukt);
+			// Hier wird das alte ProduktObjekt durch changingProdukt ersetzt;
+			deleteAndUpdateChangingProduktToKunden(testSchleife, scan, eingabeString, changingProdukt);
+		}
+	}
+
+	public static Produkt editProdukt(boolean testSchleife, Scanner scan, String eingabeString,
+			Produkt changingProdukt) {
+		/*
+		 * Der User kann durch ZahlenEingabe entscheiden was er veraendern will; Durch
+		 * Setter werden die Eigenschaften des changingProduktObjektes veraendert; Wenn
+		 * der User etwas gewaehlt hat und es durch den Setter veraendert wurde, wird
+		 * solange er nicht [0]Abbruch waehlte, eine whileSchleife aktiviert, welche die
+		 * AuswahlMoeglichkeiten erneut anzeigt; Wenn der User [0]Abbruch waehlt, wird
+		 * diese whileSchleife verlassen und das changingProduktObjekt wird returnt;
+		 */
+		System.out.println("Nun kannst du waehlen, was du editieren willst");
+		while (!eingabeString.equals("0")) {
+			do {
+				System.out.println("[0]Abbruch\n[1]Produktnummer\n[2]Produktname\n[3]Produktpreis\n[4]ProduktMHD");
+				eingabeString = scan.nextLine();
+				testSchleife = FehlerTest.tryCatchInteger(eingabeString, true, true, 0, 4, false, false, false, false,
+						false, false);
+			} while (testSchleife == true);
+
+			if (eingabeString.equals("1")) {
+				System.out.println("NEU");
+				changingProdukt.produktNummer = setProduktNummer(testSchleife, eingabeString, scan);
+			}
+			if (eingabeString.equals("2")) {
+				System.out.print("NEU");
+				changingProdukt.produktNameString = setProduktNameString(testSchleife, eingabeString, scan);
+			}
+			if (eingabeString.equals("3")) {
+				System.out.print("NEU");
+				changingProdukt.produktPreis = setProduktPreis(testSchleife, eingabeString, scan);
+			}
+			if (eingabeString.equals("3")) {
+				System.out.print("NEU");
+				changingProdukt.produktMHD = setProduktMHD(testSchleife, scan, eingabeString);
+			}
+			System.out.println("Produktnummer: " + changingProdukt.produktNummer + " Produktname: "
+					+ changingProdukt.produktNameString + " Produktpreis: " + changingProdukt.produktPreis
+					+ " ProduktMHD: " + changingProdukt.produktMHD);
+		}
+
+		return changingProdukt;
+	}
+
+	public static void deleteAndUpdateChangingProduktToKunden(boolean testSchleife, Scanner scanner,
+			String eingabeString, Produkt changingProdukt) {
+		/*
+		 * In dieser Methode werden in geschachtelten forEachSchleifen alle Kunden nach
+		 * dem editierten ProduktObjekt durchsucht; Wenn eine Kunde dieses ProduktObjekt
+		 * besitzt, wird es mit deleteProduktVonKunde() entfernt und durch das
+		 * changingProduktObjekt ersetzt;
+		 */
+		//Alle Kunden werden "angeschaut";
+		for (Integer keyKunde : KundenStrukturen.mapAlleKundenMap.keySet()) {
+			//Hier wird die mapKundenMap nach dem ProduktObjekt durchsucht;
+			for (Integer keyProdukt : KundenStrukturen.mapAlleKundenMap.get(keyKunde).mapKundenProdukte.keySet()) {
+				//Wenn das Produkt gefunden wurde, wird es erst entfernt und dann durch changingProdukt ersetzt;
+				if (keyProdukt == Integer.parseInt(eingabeString)) {
+					deleteProduktVonKunde(testSchleife, scanner, eingabeString, keyKunde);
+					KundenStrukturen.mapAlleKundenMap.get(keyKunde).mapKundenProdukte.put(changingProdukt.produktNummer,
+							changingProdukt);
+				}
+			}
+		}
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((fehlerTest == null) ? 0 : fehlerTest.hashCode());
+		result = prime * result + produktMHD;
+		result = prime * result + ((produktNameString == null) ? 0 : produktNameString.hashCode());
+		result = prime * result + produktNummer;
+		long temp;
+		temp = Double.doubleToLongBits(produktPreis);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produkt other = (Produkt) obj;
+		if (fehlerTest == null) {
+			if (other.fehlerTest != null)
+				return false;
+		} else if (!fehlerTest.equals(other.fehlerTest))
+			return false;
+		if (produktMHD != other.produktMHD)
+			return false;
+		if (produktNameString == null) {
+			if (other.produktNameString != null)
+				return false;
+		} else if (!produktNameString.equals(other.produktNameString))
+			return false;
+		if (produktNummer != other.produktNummer)
+			return false;
+		if (Double.doubleToLongBits(produktPreis) != Double.doubleToLongBits(other.produktPreis))
+			return false;
+		return true;
 	}
 
 	// Hier kommen setter und getter
@@ -178,51 +409,10 @@ public class Produkt {
 		return produktMHD;
 	}
 
-	
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + produktIndex;
-		result = prime * result + produktMHD;
-		result = prime * result + ((produktNameString == null) ? 0 : produktNameString.hashCode());
-		result = prime * result + produktNummer;
-		long temp;
-		temp = Double.doubleToLongBits(produktPreis);
-		result = prime * result + (int) (temp ^ (temp >>> 32));
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Produkt other = (Produkt) obj;
-		if (produktIndex != other.produktIndex)
-			return false;
-		if (produktMHD != other.produktMHD)
-			return false;
-		if (produktNameString == null) {
-			if (other.produktNameString != null)
-				return false;
-		} else if (!produktNameString.equals(other.produktNameString))
-			return false;
-		if (produktNummer != other.produktNummer)
-			return false;
-		if (Double.doubleToLongBits(produktPreis) != Double.doubleToLongBits(other.produktPreis))
-			return false;
-		return true;
-	}
-
 	@Override
 	public String toString() {
 		return "Produktname:" + produktNameString + ", Produktnummer: " + produktNummer + ", Produktpreis: "
-				+ produktPreis + ", ProduktMHD in Tagen: " + produktMHD + ", Produktindex: " + produktIndex;
+				+ produktPreis + ", ProduktMHD in Tagen: " + produktMHD;
 	}
 
 }
